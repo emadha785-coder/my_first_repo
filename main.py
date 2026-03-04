8d48e63fd8df858677a3d9a927afcf6316d0cec0
Emad hamed <eamdoff20@gmail.com>
2026-03-04 14:41:39 -0500

    NEW THINGS ADDED TO main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ class Person():
 
     def showInfo(self):
         return f"Your name is {self.name} and your age is {self.age}"
-
+    def age_in_days(self):
+        return self.age * 365
 
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
@@ -13,3 +14,4 @@ age = int(input("Enter your age: "))
 p1 = Person(name, age)
 
 print(p1.showInfo())
+print(f"Your age in days is {p1.age_in_days()}")
